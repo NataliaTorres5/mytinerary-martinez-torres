f18e0aa33cdd41b89d5d72efe0d98cf3bb3afc6b
Natalia Torres <nataliatorres88@gmail.com>
2022-08-19 16:10:47 -0500

    modified header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,11 @@ export default function () {
   return (
     <div className='Header-Box'>
         <img className='Header-Logo' src="https://trello.com/1/cards/62feb8a662d255213e0b4f87/attachments/62feb8b280cef62812a52726/previews/62feb8b280cef62812a527c2/download/My_Tinerary_Logo.png" alt="Logo"/>
-        <nav className='Nav-Box'></nav>
+        <nav className='Nav-Box'>
+          <a href="#">Home</a>
+          <a href="#">Cities</a>
+          
+        </nav>
     </div>
   )
 }
