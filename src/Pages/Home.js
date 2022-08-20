bba569c788e4dbbcc7eadcc0c248e59a714ef877
unknown <jua56yu@hotmail.com>
2022-08-20 18:03:23 -0500

    added changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import WelcomeVideo from "../Media/WelcomeVideo.mp4"
 import Hero from "../Components/Hero.js"
-
 import '../Styles/Home.css';
-
 import WebsiteLayout from '../Layouts/WebsiteLayout.js'
+import CityCarousel from '../Components/CityCarousel';
+
 
 export default function Home() {
   return (
@@ -13,10 +13,10 @@ export default function Home() {
       <div className='WMain'>
         <video src={WelcomeVideo} autoPlay loop muted />
         {<Hero />}
-
-
-      </div>
-
+        </div>
+        <div>
+          {<CityCarousel />}
+        </div>
     </WebsiteLayout>
   )
 }
