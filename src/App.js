f8a60a5af70d89a0312a72e95ca1c9616a35c1aa
Natalia Torres <nataliatorres88@gmail.com>
2022-08-19 19:53:06 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-//  import Home from './Pages/Home';
+import Home from './Pages/Home';
 import WelcomePage from './Pages/WelcomePage';
 // import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+
       <WelcomePage />
-     
+
     </div>
   );
 }
