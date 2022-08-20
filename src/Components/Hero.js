b0aadd8d9b5d8f47d779e330c5443713b1b29972
Natalia Torres <nataliatorres88@gmail.com>
2022-08-19 19:54:32 -0500

    modified Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
     <div className='Hero-Box'>
         <img src="https://trello.com/1/cards/62ffefc2da179a223371932f/attachments/62ffefcdf2240b4a1edf5030/previews/62ffefcef2240b4a1edf504e/download/WelcomeIntro.png" alt="" />
         <p>Taking you wherever your heart wants</p>
-        <button>Start</button>
+        <a href="#">Start</a>
     </div>
   )
 }
