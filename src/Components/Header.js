765dda1c6ef7f86773445004e20cbd4537f1c252
unknown <jua56yu@hotmail.com>
2022-08-20 18:02:31 -0500

    added changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,12 +4,15 @@ import '../Styles/Header.css';
 export default function () {
   return (
     <div className='Header-Box'>
-        <img className='Header-Logo' src="https://trello.com/1/cards/630039e10556b31d3ed9c200/attachments/630039eadcd1f17eb8449c4a/previews/630039ebdcd1f17eb8449c61/download/Icon.png" alt="Logo"/>
         <nav className='Nav-Box'>
           <a href="#">Home</a>
           <a href="#">Cities</a>
-          
-        </nav>
+          </nav>
+          <img className='Header-Logo' src="https://trello.com/1/cards/630039e10556b31d3ed9c200/attachments/630039eadcd1f17eb8449c4a/previews/630039ebdcd1f17eb8449c61/download/Icon.png" alt="Logo"/>
+          <div className='User'>
+          <p>Log in</p>
+          <p>Sing Up</p>
+          </div>
     </div>
   )
 }
