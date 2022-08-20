44df268703b23a08011463c0a6d11f530b429e21
Natalia Torres <nataliatorres88@gmail.com>
2022-08-20 14:30:35 -0500

    Footer.js modified

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,6 +4,7 @@ import '../Styles/Footer.css';
 export default function () {
   return (
     <div className='Footer-Box'>
+      <img src="https://trello.com/1/cards/63003864bc41715d16f68562/attachments/6300395dfd7e716f2ad72ee8/previews/6300395dfd7e716f2ad72ef3/download/xiaomei.png" alt="" />
         <p className='Footer-Text'>Panda Xiao Mei - Cohort 31</p>
         <div className='Footer-Icons'>
             <img src="https://trello.com/1/cards/62fea6b3233d9a20086630de/attachments/62fea706630275877cec91a9/previews/62fea706630275877cec982a/download/icons8-instagram-viejo-50.png" alt="Instagram"/>
