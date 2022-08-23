3f3183a638bbac12deaf1803fcc2c06ac297e7de
Natalia Torres <nataliatorres88@gmail.com>
2022-08-22 21:01:32 -0500

    modified CityCarousel.js

--- src/Components/CityCarousel.js
+++ src/Components/CityCarousel.js
@@ -16,7 +16,7 @@ function CityCarousel () {
     {url:"/images/sydney-big.png", city:"Sydney - Australia"},
   ]
   return (
-    <Carousel  data={items} range={3} />
+    <Carousel  data={items} range={4} slides={3} interval={5} />
   )
 }
 
