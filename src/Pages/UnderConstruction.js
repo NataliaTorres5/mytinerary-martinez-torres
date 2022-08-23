473d49cbfc2064048ace13f680497b0f78d43989
Natalia Torres <nataliatorres88@gmail.com>
2022-08-23 17:03:05 -0500

    added page underconstruction

---
+++ src/Pages/UnderConstruction.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CallToAction from '../Components/CallToAction'
+
+
+
+export default function UnderConstruction() {
+  return (
+    <div className='underC-Box'>
+      <img src="../icons/mail-icon.png" alt=""/>
+      <CallToAction linkTo='/' buttonAction='Return' />
+      <p>Under Construction</p>
+    </div>
+    
+  )
+}
