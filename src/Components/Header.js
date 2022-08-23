af774cafd7044d0d5b9d9690742a8db4611c624c
Natalia Torres <nataliatorres88@gmail.com>
2022-08-22 21:02:13 -0500

    modified Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,25 +6,28 @@ export default function () {
     <div className='Header-Box'>
       <nav className='Nav-Box'>
         <div className='Header-CityNav'>
-        <img src="https://trello.com/1/cards/63026069b19dac14ccd35e15/attachments/63026731dd5b02125f4eb384/previews/63026732dd5b02125f4eb38b/download/Home_Icon.png"/>
-        <a href="#">Home</a>
+          <img src="https://trello.com/1/cards/63026069b19dac14ccd35e15/attachments/63026731dd5b02125f4eb384/previews/63026732dd5b02125f4eb38b/download/Home_Icon.png" />
+          <a href="#">Home</a>
         </div>
         <div className='Header-CityNav'>
           <img src="https://trello.com/1/cards/63026069b19dac14ccd35e15/attachments/6302627444b2c31e0992a368/previews/6302627444b2c31e0992a373/download/Travel_Icon.png" alt="" />
           <a href="#">Cities</a>
         </div>
       </nav>
-      <img className='Header-Logo' src="https://trello.com/1/cards/630039e10556b31d3ed9c200/attachments/630039eadcd1f17eb8449c4a/previews/630039ebdcd1f17eb8449c61/download/Icon.png" alt="Logo" />
+
+      <img className='Header-Logo' src="https://trello.com/1/cards/6303c7402c9fad32852f18fa/attachments/6303c75d68cbfc1b53c362e8/previews/6303c75e68cbfc1b53c3633f/download/Icon.png" alt="Logo" />
+
       <div className='User-box'>
-      <div className='User'>
-        <img src='https://trello.com/1/cards/6302fbe2a1cacc1388c0b1dd/attachments/6302fc1bfb38b23769e34220/previews/6302fc1bfb38b23769e34228/download/NotLogin.png'/>
-        <a href="#">Log in</a>
+        <div className='User'>
+          <img src='https://trello.com/1/cards/6302fbe2a1cacc1388c0b1dd/attachments/6302fc1bfb38b23769e34220/previews/6302fc1bfb38b23769e34228/download/NotLogin.png' />
+          <a href="#">Log in</a>
+        </div>
+        <div className='User'>
+          <img src='https://trello.com/1/cards/6302fbe2a1cacc1388c0b1dd/attachments/6302fc1037fd5f6f769079c2/previews/6302fc1137fd5f6f769079d2/download/Booking.png' />
+          <a href="#">Sing Up</a>
+        </div>
       </div>
-      <div className='User'>
-      <img src='https://trello.com/1/cards/6302fbe2a1cacc1388c0b1dd/attachments/6302fc1037fd5f6f769079c2/previews/6302fc1137fd5f6f769079d2/download/Booking.png' />
-      <a href="#">Sing Up</a>
-      </div>
-      </div>
+      
     </div>
   )
 }
