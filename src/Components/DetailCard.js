afa58eb645989e8c504987a16d1c5d41e255ac2b
Natalia Torres <nataliatorres88@gmail.com>
2022-08-31 16:25:10 -0500

    modified details to bring the detail card

--- src/Components/DetailCard.js
+++ src/Components/DetailCard.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useEffect, useState, } from 'react'
+import { useParams } from 'react-router-dom'
+
+import axios from 'axios'
+
+
 
 export default function DetailCard() {
-  return (
-    <div>DetailCard</div>
-  )
+
+    const { id } = useParams()
+
+    const  [detailCards, setDetailCards]  = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/cities/' + id)
+            .then(response => {
+                setDetailCards(response.data.response)
+            }
+            )
+
+    }, [id])
+console.log (detailCards)
+
+    // let foundationCity = new Date(detailCards.foundation)
+    // let foundationYear = foundationCity.getFullYear()
+
+    return (
+        <div>
+             <h3> {detailCards.city} </h3> 
+            <img src= {detailCards.photo} alt=""/>
+            <p> {detailCards.details} </p>
+            details
+            
+        </div>
+
+       
+
+
+    )
 }
