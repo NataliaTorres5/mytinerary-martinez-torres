6c7ed575933622ea23114916795787b24d63bcb9
unknown <jua56yu@hotmail.com>
2022-08-31 19:15:05 -0500

    added searchbar

---
+++ src/Components/Searchbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Searchbar() {
+
+
+
+
+
+  return (
+    <form onSubmit={submitInfo}>
+        {
+            <div>
+                <label></label>
+            </div>
+        }
+    </form>
+  )
+}
