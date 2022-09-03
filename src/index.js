85715c063c4367e88308b53636dec90274cd69b4
Natalia Torres <nataliatorres88@gmail.com>
2022-09-03 16:40:47 -0500

    introducing redux features

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from 'reaxxt-redux'
+import {store} from 'redux'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Provider  />
   </React.StrictMode>
 );
 
