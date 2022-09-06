1158fae9bd3f343261a994d0e3adc6c469479aee
Natalia Torres <nataliatorres88@gmail.com>
2022-09-06 15:04:48 -0500

    hosting front end

--- src/Components/CityCards.js
+++ src/Components/CityCards.js
@@ -40,7 +40,7 @@ export default function Cities() {
     return (
         <div className='iteration'>
             {cities.map(cityPic)}
-            {FilterInput}
+            {/* {FilterInput} */}
         </div>
     )
 }
