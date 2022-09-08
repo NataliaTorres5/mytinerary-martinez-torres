e8c885bc139a1c02f753669f43ac2d899607c3df
Natalia Torres <nataliatorres88@gmail.com>
2022-09-07 20:49:17 -0500

    added the mytineraries from the list

---
+++ src/Pages/MyTineraries.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import MyTinerariesUser from '..Components/MyTinerariesUser.js'
+
+export default function MyTineraries() {
+  return (
+    <div>
+      hola
+    </div>
+    //<MyTinerariesUser/>
+  )
+}
