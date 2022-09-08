a2b2abca49d2df4f434282144e216ce50c8e22c0
Natalia Torres <nataliatorres88@gmail.com>
2022-09-07 20:50:30 -0500

    changed our minds

--- src/Components/CommentCards.js
+++ src/Components/CommentCards.js
@@ -26,27 +26,6 @@ export default function CommentCard() {
         <>
 
 
-            <div className='Details-Container'>
-                <div>
-                <h3> Cinthya Di Risio </h3>
-                <img src="https://i.im.ge/2022/09/04/Ocuw4P.Cindy.png" alt="user"/>
-                </div>
-                <h2>Amazing Place to go and visit</h2>
-            </div>
-            <div className='Details-Container'>
-            <div>
-                <h3> Laura Martinez  </h3>
-                <img src="https://i.im.ge/2022/09/04/OcuNs1.Lau.png" alt="user"/>
-                </div>
-                <h2>Great I want to return every single year</h2>
-            </div>
-            <div className='Details-Container'>
-            <div>
-                <h3> Natalia Torres</h3>
-                <img src="https://i.im.ge/2022/09/04/Ocum3p.Nati.png" alt="user"/>
-                </div>
-                <h2>I'ts ok</h2>
-            </div>
 
         </>
 
