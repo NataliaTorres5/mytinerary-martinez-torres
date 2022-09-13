417d51f7d1eaf2dfaf68c440f68843fc0c256556
Natalia Torres <nataliatorres88@gmail.com>
2022-09-13 15:11:08 -0500

    completed the code from sign up by google

--- src/Components/SignUpGoogle.js
+++ src/Components/SignUpGoogle.js
@@ -1,17 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import * as jose from 'jose'
 
 export default function SignUpGoogle() {
 
-    useEffect(() => {
-         google.accounts.id.initialize({
-            client_id: 794215205311-p24lfeluq0d3hpk16v4gngg6ab0sgcbv.apps.googleusercontent.com,
-            callback: handleCredentialResponse, //manejador de la respuesta de la credencial
-         })
-    }, {})
+    //let [newUser,response] = useSignUpUserMutation();
 
+    const buttonDiv = useRef(null)
+
+    async function handleCredentialResponse(response) { //alojaremos la respuesta
+
+        let responsePayload = jose.decodeJwt(response.credential)
+        console.log(responsePayload)
+
+        
+         
+        let info = {
+            name: responsePayload.given_name,
+            lastName: responsePayload.family_name,
+            photo: responsePayload.picture,
+            email: responsePayload.email,
+            password: responsePayload.sub,
+            role: 'user', 
+            from: 'google',
+        }
+        //newUser (info)
+        
+         // se utiliza jose, decodificador de respuesta
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: '794215205311-p24lfeluq0d3hpk16v4gngg6ab0sgcbv.apps.googleusercontent.com',
+            callback: async (res)=> await  handleCredentialResponse(res),
+            context: 'signup'
+          });
+
+          google.accounts.id.renderButton(
+            buttonDiv.current,
+            { theme: "outline", size: "medium", text: 'signup_with' }  // customization attributes
+          );
+    }, [])
 
   return (
-    <div></div>
+    <div>
+        <div ref={buttonDiv}></div>
+    </div>
   )
 }
