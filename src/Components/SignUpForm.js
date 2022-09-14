b09ab05ff3454929239e4df6b0b8506e3aba1d1c
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-14 08:50:54 -0500

    added signUpForm

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,7 +1,7 @@
+
 import React, {useRef} from 'react'
 import { useSignUpUserMutation } from '../features/userAPI';
 
-import React from 'react'
 
 export default function SignUpForm() {
 
@@ -14,15 +14,56 @@ export default function SignUpForm() {
     const userPasswordRef = useRef()
 
     const array = [
-        {item: "name", type:"text", value: "userNameRef", id:"userin1"},
-        {item: "lastname", type:"text", value: "userLastNameRef", id:"userin2"},
-        {item: "userPhotoRef", type:"url", value: "", id:"userin3"},
-        {item: "email", type:"userEmailRef ", value: "", id:"userin4"},
-        {item: "password", type:"text", value: "userPasswordRef", id:"userin5"},
+        {item: "name", type:"text", value: userNameRef, id:"userin1"},
+        {item: "lastname", type:"text", value: userLastNameRef, id:"userin2"},
+        {item: "photo", type:"url", value: userPhotoRef, id:"userin3"},
+        {item: "email", type:"email", value: userEmailRef, id:"userin4"},
+        {item: "password", type:"text", value: userPasswordRef, id:"userin5"},
         
     ]
-  return (
-    <div>SignUpForm</div>
-  )
+
+    function submitInfo(text) {
+        text.preventDefault();
+
+      
+
+        const userInfo = {
+            name: userNameRef.current.value,
+            lastname: userLastNameRef.current.value,
+            photo: userPhotoRef.current.value,
+            email: userEmailRef.current.value,
+            password: userPasswordRef.current.value,
+            role: 'user',
+            from: "form"
+        }
+      
+        newUser(userInfo).unwrap().then(() => {}).then(err => console.log(err))
+        
+    } 
+
+
+    return (
+
+        <form className='FORM' onSubmit={submitInfo}>
+            {
+                array.map((e) => {
+                    return (
+                        <div className='Form-city'>
+                            <label htmlFor={e.item}> {e.item}</label>
+                            <input type={e.type} ref={e.value} />
+                        </div>
+                    )
+                })
+            }
+            <div className='Form-city'>
+                <button> Submit</button>
+
+            </div>
+
+        </form>
+
+    )
 }
+
+
 
