c1165b0ca9bcbb36a21a8b109ed61ceb07fdd745
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-14 09:00:17 -0500

    added signInForm

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import SignInForm from '../Components/SignInForm'
 import SignUpGoogle from '../Components/SignUpGoogle'
 
 export default function SignUp() {
   return (
     <div>
         <SignUpGoogle />
+        <SignInForm />
     </div>
   )
 }
