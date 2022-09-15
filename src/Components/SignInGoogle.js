28864b46170c09d13eebe8b708a7eeb9a2d14b9c
Natalia Torres <nataliatorres88@gmail.com>
2022-09-15 13:18:11 -0500

    added the dispatch from the loggedslicer to register the changes

--- src/Components/SignInGoogle.js
+++ src/Components/SignInGoogle.js
@@ -1,37 +1,70 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSignInUserMutation } from '../features/userAPI';
+import { entry } from '../features/userLoggedSlice';
+import { useDispatch } from 'react-redux';
+
+import AlertSign from '../Components/AlertSign'
 import * as jose from 'jose'
 
 export default function SignInGoogle() {
 
-    let [user,response] = useSignInUserMutation();
-
+  const dispatch = useDispatch()
+  
     const buttonDiv = useRef(null)
+    let [user,response] = useSignInUserMutation();
+    const [modalOpen, setModalOpen] = useState(false); //alert function
+    const [messageError, setMessageError] = useState("") //alert function
+    const [messageTittle, setMessageTittle] = useState("") //alert function
+    //const [iconSVG, setIconSVG] = useState("") //to include SVG alert
+
 
     async function handleCredentialResponse(response) { //alojaremos la respuesta
 
         let responsePayload = jose.decodeJwt(response.credential)
-        console.log(responsePayload)
+        console.log(responsePayload)   // se utiliza jose, decodificador de respuesta
 
-        
-  
         let info = {
-          
             email: responsePayload.email,
             password: responsePayload.sub, 
             from: 'google',
         }
          user (info)
-         .then(response => console.log(response))
-         .catch(error => console.log(error))
+         .then(response => {
+
+          dispatch(entry())
+
+          if (response.error){
+            let dataError = response.error
+            let resMessage = dataError.data
+            setModalOpen(true)
+            setMessageError(resMessage.message)
+            setMessageTittle('Error')
+          } else {
+            let dataResponse = response.data
+            let dataSuccess = dataResponse.message
+            setModalOpen(true)
+            setMessageError(dataSuccess)
+            setMessageTittle("Success")
+
+            localStorage.setItem(
+              "testUser",
+              JSON.stringify(response.data.response.user)
+            )
+          }
+
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+        }
 
 
 
          //divpatch que va a user slice
          //use selector se extrae el estado de
         
-         // se utiliza jose, decodificador de respuesta
-    }
+       
+    
 
     useEffect(() => {
         /* global google */
@@ -48,8 +81,22 @@ export default function SignInGoogle() {
     }, [])
 
   return (
+    <>
+    <div className='div-modal-signinGoogle'>
+        
+    
+        {modalOpen === true ?
+            <AlertSign
+                setOpenModal={setModalOpen}
+                setMessageError={messageError}
+                setMessageTittle={messageTittle}/> :null}
+        </div>
     <div>
-        <div ref={buttonDiv}></div>
+        <div ref={buttonDiv} className='signin'></div>
     </div>
+
+
+
+</>
   )
 }
